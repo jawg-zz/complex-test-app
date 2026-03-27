ca0c016dbd73a2bc6923ac5e0cc05c4bba2943c5
jawg-zz <mosetigeorge@gmail.com>
2026-03-27 21:36:03 +0000

    Fix TypeScript error in worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -2,7 +2,7 @@ import Bull from 'bull';
 import Redis from 'ioredis';
 
 const redisHost = process.env.REDIS_HOST || 'localhost';
-const redisPort = parseInt(process.env.REDIS_PORT) || 6379;
+const redisPort = parseInt(process.env.REDIS_PORT || '6379');
 
 const redis = new Redis({
   host: redisHost,
